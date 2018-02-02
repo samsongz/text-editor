c43cd8297941198f0229151102e371130d4562df
Sam Meyer <sam.meyer@procore.com>
2018-02-02 13:19:23 -0800

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './TextEditor';
-import registerServiceWorker from './registerServiceWorker';
 
 const mount = (node, props) => {
   ReactDOM.render(<App {...props} />, node);
